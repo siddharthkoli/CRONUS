c823469021e5fda237ce88deff8df0fe195188f8
siddharthkoli <siddharthkoli2401@gmail.com>
2021-05-16 21:54:48 +0530

    fixed analyzeCommands. added logging

--- notifications/TelegramNotification.js
+++ notifications/TelegramNotification.js
@@ -5,6 +5,7 @@ const users = require('./telegramConfig');
 const fs = require('fs');
 const path = require('path');
 const TermDeposit = require('../jobs/TermDeposit');
+const Logger = require('../Logger/Logger');
 
 class TelegramNotification extends BaseNotification {
     constructor(id) {
@@ -29,21 +30,28 @@ class TelegramNotification extends BaseNotification {
                 const tn = new TelegramNotification(user.message.chat.id);
                 tn.sendNotification(tn.generateWelcomeMessage());
                 console.log(`New User added: ${user.message.from.first_name}`);
+                Logger.logNewUser({ user: user.message.from.first_name, chatId: user.message.chat.id });
             }
         })
     }
 
     static analyzeCommands(data) {
         data.forEach((update) => {
-            if (!update.message.entities) return;
+            if (!update.message.text) continue;
+            if (!update.message.entities && update.message.text) {
+                if (update.message.text == 'test') {
+                    console.log(`Working`);
+                    new TelegramNotification(update.message.from.id).sendNotification('Working');
+                }
+                Logger.logMessage({ updateId: update.update_id, user: update.message.chat.first_name, chatId: update.message.chat.id, msg: update.message.text });
+                continue;
+            }
             switch (update.message.text) {
+                // all commands will come here.
                 case '/my_reminders':
                     const tn = new TelegramNotification(update.message.chat.id);
                     tn.executeMyReminders();
-                    break;
-
-                case 'test':
-                    console.log("Working");
+                    Logger.logMessage({ user: update.message.chat.first_name, chatId: update.message.chat.id, msg: update.message.text });
                     break;
             };
         });
@@ -58,23 +66,9 @@ class TelegramNotification extends BaseNotification {
             res.on('data', function (data) {
                 data = JSON.parse(data);
                 if (!data.result) return;
-                console.log(`${data}`); 
+                console.log(`${data}`);
                 TelegramNotification.checkAndAddNewUsers(data);
-                // data.result.forEach((user) => {
-                //     if (!users.includes(user.message.from.first_name)) {
-                //         users.push(user.message.from.first_name);
-                //         fs.writeFileSync(
-                //             __dirname + '/telegramConfig.js',
-                //             `let users = ${JSON.stringify(users)};\nmodule.exports=users;`
-                //         );
-                //         fs.appendFileSync(
-                //             path.join(__dirname, '../', `${process.env.NODE_ENV}.env`),
-                //             `\n${user.message.from.first_name.toUpperCase()}_CHAT_ID=${user.message.chat.id}`
-                //         );
-                //         const tn = new TelegramNotification(user.message.chat.id);
-                //         tn.sendNotification(tn.generateWelcomeMessage());
-                //     }
-                // })
+                TelegramNotification.analyzeCommands(data.result);
             });
         });
         getReq.end();
