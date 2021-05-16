f5d42851ff60c82fcaca38437bc3d8f13186be1f
siddharthkoli <siddharthkoli2401@gmail.com>
2021-05-16 22:03:07 +0530

    fixed logging

--- Logger/Logger.js
+++ Logger/Logger.js
@@ -14,38 +14,28 @@ module.exports = class Logger {
     static logTermDeposit(data) {
         let content = `${id} ${Date.now()} ${data.to}\n`;
         id++;
-        const log = new Logger();
-        log.logToFile('Term_Deposit.txt', content);
-        delete log;
+        new Logger().logToFile('Term_Deposit.txt', content);
     }
 
     static logNationalSavingsCertificate(data) {
         let content = `${id} ${Date.now()} ${data.to}\n`;
         id++;
-        const log = new Logger();
-        log.logToFile('National_Savings_Certificate.txt', content);
-        delete log;
+        new Logger().logToFile('National_Savings_Certificate.txt', content);
     }
 
     static logMonthlyIncomeScheme(data) {
         let content = `${id} ${Date.now()} ${data.to}\n`;
         id++;
-        const log = new Logger();
-        log.logToFile('Monthly_Income_Scheme.txt', content);
-        delete log;
+        new Logger().logToFile('Monthly_Income_Scheme.txt', content);
     }
 
     static logNewUser(data) {
         let content = `${Date.now()} ${data.user} ${data.chatId}\n`;
-        const log = new Logger();
-        log.logToFile('Telegram_Updates.txt', content);
-        delete log;
+        new Logger().logToFile('Telegram_Updates.txt', content);
     }
 
     static logMessage(data) {
         let content = `${Date.now()} ${data.update_id} ${data.user} ${data.chatId} ${data.msg}\n`;
-        const log = new Logger();
-        log.logToFile('Telegram_Updates.txt', content);
-        delete log;
+        new Logger().logToFile('Telegram_Updates.txt', content);
     }
 }
